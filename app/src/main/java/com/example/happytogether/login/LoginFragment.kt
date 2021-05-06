fc96da0b758aff743bd27833ada18f7aacc57024
leo <leonard_tetrick@hotmail.com>
2021-05-06 20:59:16 +0700

    soundpool

--- app/src/main/java/com/example/happytogether/login/LoginFragment.kt
+++ app/src/main/java/com/example/happytogether/login/LoginFragment.kt
@@ -1,19 +1,22 @@
 package com.example.happytogether.login
 
-import android.content.Intent
-import android.os.Bundle
 import androidx.fragment.app.Fragment
+import android.content.Intent
+import android.media.AudioManager
+import android.media.SoundPool
+import android.os.Build
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import android.widget.Toast.LENGTH_SHORT
 import com.example.happytogether.MainActivity
 import com.example.happytogether.R
 import com.example.happytogether.data.Data
 import com.example.happytogether.data.EXTRA_DATA
 import com.example.happytogether.databinding.FragmentLoginBinding
 
+private var sp: SoundPool? = null
+private var soundID = 0
 class LoginFragment : Fragment() {
 
     private var _binding: FragmentLoginBinding? = null
@@ -24,6 +27,9 @@ class LoginFragment : Fragment() {
         val view = binding.root
         var nama = binding.email.text
         binding.btnLogin.setOnClickListener{
+            if (soundID != 0) {
+                sp?.play(soundID, 0.99f, .99f, 1, 0, .99f)
+            }
             var intent = Intent(this.activity, MainActivity::class.java)
             if (nama.toString() == "") {
                 var parcel = Data()
@@ -53,5 +59,38 @@ class LoginFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            createNewSoundPool()
+        }
+        else {
+            createOldSoundPool()
+        }
+        sp?.setOnLoadCompleteListener { soundPool, id, status ->
+            if (status != 0) {
+
+            }
+            else {
+
+            }
+        }
+        soundID = sp?.load(activity, R.raw.ringtone, 1) ?: 0
+    }
+    private fun createNewSoundPool() {
+        sp = SoundPool.Builder()
+                .setMaxStreams(15)
+                .build()
+    }
+    private fun createOldSoundPool() {
+        sp = SoundPool(15, AudioManager.STREAM_MUSIC, 0)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        sp?.release()
+        sp = null
     }
 }
